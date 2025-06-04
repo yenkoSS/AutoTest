4e90ae3be1b4d8b0fec5a48d696cd367e48f4420
Stefan Petru-Cornel <petrucornel.stefan@gmail.com>
2025-06-05 00:03:46 +0300

    add intersection observer

--- js/script.js
+++ js/script.js
@@ -78,3 +78,35 @@ document.querySelectorAll(".card-faq").forEach((card) => {
     }
   });
 });
+
+const xOffsets = document.querySelectorAll(".offsetX");
+const yOffsets = document.querySelectorAll(".offsetY");
+
+const offsetXObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("offset-x0", entry.isIntersecting);
+      if (entry.isIntersecting) offsetXObserver.unobserve(entry.target);
+    });
+  },
+  {
+    root: null, // default is the viewport
+    rootMargin: "0px 0px -200px 0px", // top, right, bottom, left
+  }
+);
+
+const offsetYObserver = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      entry.target.classList.toggle("offset-y0", entry.isIntersecting);
+      if (entry.isIntersecting) offsetYObserver.unobserve(entry.target);
+    });
+  },
+  {
+    root: null, // default is the viewport
+    rootMargin: "0px 0px -200px 0px", // top, right, bottom, left
+  }
+);
+
+xOffsets.forEach((xOffset) => offsetXObserver.observe(xOffset));
+yOffsets.forEach((yOffset) => offsetYObserver.observe(yOffset));
